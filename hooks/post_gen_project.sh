627a7df2a8cbb0e13c0946390db804c35b150246
Jessica Alvares Jordao de Oliveira <jessica.oliveira@dextra-sw.com>
2021-02-11 12:56:31 -0300

    Adds first templat

---
+++ hooks/post_gen_project.sh
@@ -0,0 +1,5 @@
+#! /usr/bin/env bash
+
+set -eu
+mv * ..
+rm -rf ../root
---
+++ hooks/pre_gen_project.sh
@@ -0,0 +1,6 @@
+#! /usr/bin/env bash
+
+rmdir definitions/
+rmdir includes/
+rm dataform.json || true
+rm environments.json || true
