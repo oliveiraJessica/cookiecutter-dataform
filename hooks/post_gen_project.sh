4a12f4bce2f0f6ee0c6d1e1b7a7f7e7f1049b51a
Valeria Silva <silvvavaleria@gmail.com>
2021-02-12 15:41:58 -0300

    Remoçao de arquivos ocultos

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -3,14 +3,5 @@
 set -eu
 mv * ..
 rm -rf ../root
-#! /usr/bin/env bash
-
-set -eu
-mv * ..
-rm -rf ../root
-rm -f "../definitions/{%- if cookiecutter.enum_folders == "yes" -%} 00_ {% endif %}source/.keep"
-rm -f "../definitions/{%- if cookiecutter.enum_folders == "yes" -%} 01_ {% endif %}staging/.keep"
-rm -f "../definitions/{%- if cookiecutter.enum_folders == "yes" -%} 02_ {% endif %}tests/.keep"
-rm -f "../definitions/{%- if cookiecutter.enum_folders == "yes" -%} 03_ {% endif %}reports/.keep"
-rm -f "../includes/.keep"
-
+rm -f ../definitions/*/.keep
+rm -f ../includes/.keep
