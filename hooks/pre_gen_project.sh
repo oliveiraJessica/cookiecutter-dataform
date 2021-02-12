d596752a9e3e580af8db30b6c86e25fb7069303e
Valéria Cristina dos Santos Silva <valeria.silva-ext@anbima.com.br>
2021-02-12 09:54:13 -0300

    Removendo os arquivos

--- hooks/pre_gen_project.sh
+++ hooks/pre_gen_project.sh
@@ -1,9 +1,13 @@
 #! /usr/bin/env bash
 
-mkdir definitions/
-cd definitions/ 
+mkdir "definitions/" && cd "definitions/" 
 mkdir 00_sources/
+mkdir 01_staging/
+mkdir 02_tests/
+mkdir 03_reports/
 cd ..
-mkdir includes/
-rm dataform.json || true
-rm environments.json || true
+mkdir "includes/" && cd "includes/"
+touch constants.js
+cd ..
+rm -rf  dataform.json || true
+rm -rf environments.json || true
