901bd513a04000b0e456fc6af65381b3fd3bfe4a
Valéria Cristina dos Santos Silva <valeria.silva-ext@anbima.com.br>
2021-02-12 09:07:09 -0300

    Teste commit

--- hooks/pre_gen_project.sh
+++ hooks/pre_gen_project.sh
@@ -1,6 +1,9 @@
 #! /usr/bin/env bash
 
-rmdir definitions/
-rmdir includes/
+mkdir definitions/
+cd definitions/ 
+mkdir 00_sources/
+cd ..
+mkdir includes/
 rm dataform.json || true
 rm environments.json || true
